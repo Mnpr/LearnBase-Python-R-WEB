a21aa37abfaacab60a41eef0a0cb0de1f56e5e5c
Mnpr <acharya.sudesh366@gmail.com>
2021-03-02 18:59:43 +0100

    stack

---
+++ Python/DataStructures/stack.py
@@ -0,0 +1,51 @@
+# Stack with List
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def items_top_bottom(self):
+        top = self.items[-1]
+        bottom = self.items[0]
+        return top, bottom
+
+    def size(self):
+        return len(self.items)
+
+    def info(self):
+
+        stack_info = f"""
+        Info [ Stack ]
+        ____________________________________________
+        Stack        : {self}
+        Stack empty  : {self.is_empty()}
+        Stack Size   : {self.size()}
+        --------------------------------------------
+        # Stack Top    : {self.items[-1]}
+        # Stack Bottom : {self.items[ 0]}
+        --------------------------------------------
+        """
+        print(stack_info)
+
+# Test stack object
+stack = Stack()
+
+# Operations:
+for i in range(5):
+    stack.push(i)
+    print(f'\t{i} added to the stack')
+
+# LIFO
+print(f'\t{stack.pop()} popped out of stack')
+
+# Final Stack Info
+stack.info()
+
