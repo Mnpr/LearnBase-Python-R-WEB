4e0e8db4b92e2fa572c1f5da873b9d8d8691beb6
Mnpr <acharya.sudesh366@gmail.com>
2021-03-03 15:02:09 +0100

    fizz_buzz added

---
+++ Python/Scripts/fizz_buzz.py
@@ -0,0 +1,19 @@
+# for n = 1-100 print:
+#   fizz if n % 3 == 0
+#   buzz if n % 5 == 0
+#   fizzbuzz if n%3 == 0 and n%5 ==0
+
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i%3 == 0 and i%5==0:
+            print('‛¯¯٭Fizz٭¯¯(▫▫)¯¯٭Buzz٭¯¯’')
+        elif i%3 == 0:
+            print('‛¯¯٭Fizz٭¯¯(▫|')
+        elif i%5 == 0:
+            print('|▫)¯¯٭Buzz٭¯¯’')
+        else:
+            print(i)
+
+fizzbuzz(100)
+
+
