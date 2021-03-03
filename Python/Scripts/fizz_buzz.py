c342258d12fc2eeee15107ba3c42f0b95804b8d7
Mnpr <acharya.sudesh366@gmail.com>
2021-03-03 23:13:49 +0100

    scripts renamed

--- Python/Scripts/fizz_buzz.py
+++ Python/Scripts/fizz_buzz.py
@@ -8,12 +8,13 @@ def fizzbuzz(n):
         if i%3 == 0 and i%5==0:
             print('‛¯¯٭Fizz٭¯¯(▫▫)¯¯٭Buzz٭¯¯’')
         elif i%3 == 0:
-            print('‛¯¯٭Fizz٭¯¯(▫|')
+            print('٭Fizz٭')
         elif i%5 == 0:
-            print('|▫)¯¯٭Buzz٭¯¯’')
+            print('٭Buzz٭')
         else:
             print(i)
 
 fizzbuzz(100)
 
 
+  
